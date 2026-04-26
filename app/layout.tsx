98f66bee203b07909d882f244d8a3430e2c187ea
dimas <kontenyt1014@gmail.com>
2026-04-27 00:27:51 +0700

    chore: complete migration of app folder to root and fix imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "@/src/app/globals.css";
+import "@/app/globals.css";
 import ClientLayout from "./ClientLayout";
 
 export const metadata: Metadata = {
